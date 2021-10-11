b06f62b983f1d5923ea40087798bfc33eac572e0
sam bacha <sam@manifoldfinance.com>
2021-10-11 09:36:50 -0700

    Update env-ci.sh

--- env-ci.sh
+++ env-ci.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -x
 CI_NAME=
-if
-elif [ "$CI" = "true" ] && [ "$CI_NAME" = "codeship" ];
-elif [ "$TEAMCITY_VERSION" != "" ];
-elif [ "$CI" = "true" ] && [ "$CIRCLECI" = "true" ];
-elif [ "$CI" = "drone" ] || [ "$DRONE" = "true" ];
+if [ "$CI" = "true" ] && [ "$CIRCLECI" = "true" ];
 elif [ "$GITHUB_ACTIONS" != "" ];
 fi
 git diff --quiet HEAD^ HEAD ./
