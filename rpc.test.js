542d4464a5ff8a1c2f88c3e7b3d9de2ce9de07f6
sam bacha <sam@manifoldfinance.com>
2021-10-11 09:37:26 -0700

    response

--- rpc.test.js
+++ rpc.test.js
@@ -1,6 +1,6 @@
 const pactum = require('pactum');
 
-it('should be a teapot', async () => {
+it('should respond status', async () => {
   await pactum.spec()
     .get('https://api.staging.sushirelay.com/v1')
     .expectStatus(200);
